9569f979fb648373a97c3491f45bc8bae1ec3883
kimbumsu0312 <91525897+kimbumsu0312@users.noreply.github.com>
2026-04-20 22:39:39 +0900

    feat:player 공격 개선 및 대쉬 추가

--- Potato_Project/Assets/Project/Scripts/Character.cs
+++ Potato_Project/Assets/Project/Scripts/Character.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Character : MonoBehaviour
 {
     public float    speed;
     public bool     isDead = false;
-    protected Vector2 Move(Vector2 dir)
+    public int      health;
+    public Vector2 Move(Vector2 dir)
     {
         Vector2 nextVec = dir * speed * Time.fixedDeltaTime;
         return nextVec;
--- Potato_Project/Assets/Project/Scripts/Player.cs
+++ Potato_Project/Assets/Project/Scripts/Player.cs
@@ -1,42 +1,76 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : Character
 {
+    public PlayerStateMachine stateMachine;
     public Vector2 inputVec;
+    public Vector2 lastMoveDir;
 
-    Rigidbody2D rigid;
-    SpriteRenderer spriterRd;
+    public Rigidbody2D rigid;
+    public SpriteRenderer spriterRd;
     Animator anim;
+    public Weapon equipWeapon;
+
+    public bool isDash;
+
+    public float dashSpeed = 10f;
+    public float dashDuration = 0.2f;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriterRd = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+
+        stateMachine = new PlayerStateMachine(this);
+
+        stateMachine.AddState(new PlayerIdle());
+        stateMachine.AddState(new PlayerMove());
+        stateMachine.AddState(new PlayerDash());
+        stateMachine.AddState(new PlayerAttack());
+    }
+    void Start()
+    {
+        stateMachine.Init<PlayerIdle>();
     }
     void Update()
     {
-        if(isDead)
+        stateMachine.Update();
+
+        if (isDead)
         {
             anim.SetTrigger("Dead");
         }
     }
     void FixedUpdate()
     {
-        rigid.MovePosition(rigid.position + Move(inputVec));
+        stateMachine.FixedUpdate();
     }
 
-    void LateUpdate()
-    {
-        anim.SetFloat("Speed", inputVec.magnitude);
-        if(inputVec.x != 0)
-        {
-            spriterRd.flipX = inputVec.x < 0;
-        }
-    }
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+        if (inputVec.sqrMagnitude > 0.01f)
+        {
+            lastMoveDir = inputVec.normalized;
+        }
     }
     
+    void OnAttack(InputValue value)
+    {
+        if (!value.isPressed){
+            return;
+        }
+        stateMachine.ChangeState<PlayerAttack>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!value.isPressed || equipWeapon.isAttacking) {
+            return;
+        }
+        stateMachine.ChangeState<PlayerDash>();
+    }
+
 }
